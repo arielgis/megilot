8c578fd4abd5d6e5f66e57c687c116589a45bda0
arielgis <ariel.gispan@mail.huji.ac.il>
2025-07-30 23:05:29 +0300

    changes

--- src/dji_caltopo/main.py
+++ src/dji_caltopo/main.py
@@ -34,7 +34,7 @@ logging.basicConfig(
     format="%(asctime)s [%(levelname)s] %(name)s - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S"
 )
-logger = logging.getLogger("dji_caltopo")
+logger = logging.getLogger("main")
 
 
 # --- MQTT Drone Subscription ---
